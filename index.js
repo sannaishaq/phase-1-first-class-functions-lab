c211d3e6babd11310233105270df2fb558fe7510
Sanna Ishaq <sakinah.ishaq@gmail.com>
2022-02-27 06:49:27 -0500

    commit tree a0ad29a47e923ef3409bf29520007c9e82991b9e

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers =((array)=> {
+    let driverArray= [] 
+    for(let driver of array){
+        if(driver[0,1].array.length){
+            driverArray.push(driver)
+        } 
+    }
+    return driverArray } )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const returnFirstTwoDrivers = function() {
+//     return drivers[0,1];
+// }
+// const returnLastTwoDrivers = function (){
+//     return drivers[2,3];
+// }
+// function selectingDrivers(returnFirstTwoDrivers, returnLastTwoDrivers){
+//     return returnFirstTwoDrivers, returnLastTwoDrivers;
+// }
+// // function createFareMultiplier(num){
+// //     return 
+// // }
+  
+// // const fairDoubler = 
